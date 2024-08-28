7a6ad4b99b74da833e0de5533e1946ac6d52376f
lDeityl <mihailov_221105@mail.ru>
2024-08-28 15:48:38 +0300

    add header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('test');
+const text = 'программирования';
+let index = 0;
+let speed = 100;
+
+const typeWriter = () => {
+    if (index < text.length) {
+        document.getElementById('typingEffect').innerHTML += text[index];
+        index++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+typeWriter();
